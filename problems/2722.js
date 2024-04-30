11e0c91c901f6c16590603637e4712c5206f7bac
wikitopian <parrott.matt@protonmail.com>
2024-04-30 00:22:41 +0000

    problem 2722

---
+++ problems/2722.js
@@ -0,0 +1,28 @@
+const join = (a1, a2) => [...a1, ...a2]
+  .reduce((combined, elem) => {
+    combined[elem.id] = { ...combined[elem.id], ...elem };
+
+    return combined;
+  }, [])
+  .filter((elem) => elem !== undefined);
+
+const arr1 = [
+  {"id":1,"x":36,"d":26,"f":35},{"id":3,"c":20,"z":75}
+];
+
+const arr2 = [{"id":1,"o":48,"z":84,"y":61}];
+
+console.log(join(arr1, arr2));
+
+/*
+  i am #1, leet is #2
+
+1: {"id":1,"x":36,"d":26,"f":35}
+2: {"d":26,"f":35,"id":1,"x":36}
+
+1: {"id":3,"c":20,"z":75}
+2: {"c":20,"id":3,"z":75}
+
+1: {"id":2,"o":48,"z":84,"y":61}
+2: {"id":2,"o":48,"y":61,"z":84}
+*/
