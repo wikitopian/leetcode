cee8165c1e134c9fbda3c8290047999007cc1e9e
wikitopian <parrott.matt@protonmail.com>
2024-05-01 20:49:11 +0000

    devDependency version changes

--- problems/2724.js
+++ problems/2724.js
@@ -1,5 +1,3 @@
-// const sortBy = (array, fn) => array.map((e, i) => ({e, fn: fn(e) }));
-
 const sortBy = (array, fn) => array.sort((a, b) => fn(a) - fn(b));
 
 const arrayTest = [9, 8, 7, 6, 5, 4, 3, 2, 1];
