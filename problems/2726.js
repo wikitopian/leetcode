5d4912ab259915dd290cefd8db34aa18fad9201f
wikitopian <parrott.matt@protonmail.com>
2024-05-04 14:24:27 +0000

    problem 2726

---
+++ problems/2726.js
@@ -0,0 +1,38 @@
+class Calculator {
+  constructor(value) {
+    this.result = value;
+  }
+
+  add(value){
+    this.result += value;
+    return this;
+  }
+
+  subtract(value){
+    this.result -= value;
+    return this;
+  }
+
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+
+  divide(value) {
+    if (!value) throw('Division by zero is not allowed');
+
+    this.result /= value;
+    return this;
+  }
+
+  power(value) {
+    this.result **= value;
+    return this;
+  }
+
+  getResult() {
+    return this.result;
+  }
+}
+
+console.log(new Calculator(7).add(5).subtract(2).multiply(3).divide(2).getResult());
